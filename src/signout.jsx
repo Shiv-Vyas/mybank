e0578ebe03599cb0dbe69b3475b73db9b6b62548
Shiv-Vyas <shivniceguy@gmail.com>
2023-05-26 01:29:51 -0400

    fixed home

--- src/signout.jsx
+++ src/signout.jsx
@@ -12,7 +12,7 @@ function Signout(){
 return(
     <div>
     <Container fluid style={{padding:"0vh", display: "flex", width:"100%", padding:"0.5vh 2vh 0vh 0vh", justifyContent:"right"}}>
-        <Button style={{padding:"0.5vh 2%", fontSize:"2vh"}} variant="dark" href="/login" onClick={e=>logOut(e)}> Signout </Button>
+        <Button style={{padding:"1vh 2%", fontSize:"2vh"}} variant="dark" href="/login" onClick={e=>logOut(e)}> Signout </Button>
     </Container>
 </div> )
 
